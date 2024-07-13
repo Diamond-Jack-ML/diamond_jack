1da2bd66f0e736579dbd85902b41f3773ec7c2f9
alan <tungstensumac@gmail.com>
2024-07-12 20:07:11 -0400

    Completed confluence_query.sql

--- rag_system/confluence_query.sql
+++ rag_system/confluence_query.sql
@@ -1,42 +1,40 @@
 WITH CommentRank AS (
-  SELECT        
-    c.page_id,        
-    c.created AS comment_created,        
-    FORMAT(            
-      'Page Comment %d\nCreated On : %s\nCreated By : %s\n%s\n%s\n%s\n',            
-      ROW_NUMBER() OVER(PARTITION BY c.page_id ORDER BY c.created),            
-      CAST(c.created AS VARCHAR),            
-      c.author,            
-      '-------------------------',            
-      c.body,            
-      '-------------------------'        
-    ) AS formatted_comment    
-  FROM        
-    confluence.footer_comment c
+  SELECT
+    fc.page_id,
+    fc.version_created_at AS comment_created,
+    FORMAT(
+      'Page Comment %d\nCreated On : %s\nTitle : %s\nStatus : %s\n%s\n%s\n%s\n',
+      ROW_NUMBER() OVER(PARTITION BY fc.page_id ORDER BY fc.version_created_at),
+      CAST(fc.version_created_at AS VARCHAR),
+      fc.title,
+      fc.status,
+      '-------------------------',
+      fcv.comment_body,
+      '-------------------------'
+    ) AS formatted_comment
+  FROM
+    footer_comment fc
+  JOIN
+    footer_comment_version fcv ON fc.id = fcv.comment_id
 )
-SELECT    
-  p.id AS page_id,    
-  p.title AS page_title,    
-  p.created_at AS page_created,    
-  pv.page_body AS page_content,
-  s.id AS space_id,
-  s.name AS space_name,
-  FORMAT(        
-    '\nPage ID : %d\nCreated On : %s\nTitle : %s\n\n%s',        
-    p.id,        
-    CAST(p.created_at AS VARCHAR),        
-    p.title,        
-    LISTAGG(cr.formatted_comment, '\n') WITHIN GROUP (ORDER BY cr.comment_created)    
+SELECT
+  p.id as page_id,
+  p.title as page_title,
+  p.created_at as page_created,
+  pv.page_body as page_content,
+  FORMAT(
+    '\nPage ID : %d\nCreated On : %s\nTitle : %s\n\n%s\n\n%s',
+    p.id,
+    CAST(p.created_at AS VARCHAR),
+    p.title,
+    pv.page_body,
+    LISTAGG(cr.formatted_comment, '\n') WITHIN GROUP (ORDER BY cr.comment_created)
   ) AS page_details
-FROM    
-  confluence.page p
-LEFT JOIN    
+FROM
+  page p
+JOIN
+  page_version pv ON p.id = pv.page_id
+LEFT JOIN
   CommentRank cr ON p.id = cr.page_id
-JOIN 
-  confluence.page_version pv ON p.id = pv.page_id
-JOIN 
-  confluence.space s ON p.space_id = s.id
-WHERE 
-  p.status = 'current'
-GROUP BY    
-  p.id, p.title, p.created_at, pv.page_body, s.id, s.name;
+GROUP BY
+  p.id, p.title, p.created_at, pv.page_body;
